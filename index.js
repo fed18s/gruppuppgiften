da17d4b6b136695367126c0605ea98dceb300140
TomSygiel <tomasz.szczygiel@medieinstitutet.se>
2019-05-17 14:20:27 +0200

    Add changes commited by Melker in index.js

--- index.js
+++ index.js
@@ -16,7 +16,21 @@ db.addCollection('cats', [
   { name: 'Kitty', color: 'Grey', age: 1 },
 ]);
 
+db.addCollection('dogs', [
+  { name: 'Pluto', color: 'White', age: 3 },
+  { name: 'Rex', color: 'Gold', age: 11 },
+  { name: 'Kex', color: 'Grey', age: 1 },
+]);
+
+db.addCollection('pokemon', [
+  { name: 'Pikachu', color: 'Yellow', age: 3 },
+  { name: 'Squirtel', color: 'Gold', age: 11 },
+  { name: 'Slowpoke', color: 'Grey', age: 1 },
+]);
+
 // Setup the routes
+
+//Cats
 app.post('/cat', (req, res) => {
   if (!req.body.name) {
     console.log(req.body);
@@ -70,6 +84,118 @@ app.get('/catSearch/:key/:value', (req, res) => {
     message: 'Cat not found',
   });
 });
+
+//Dogs
+app.post('/cat', (req, res) => {
+  if (!req.body.name) {
+    console.log(req.body);
+    return res.status(400).send({
+      success: false,
+      message: 'Name is required for cat',
+    });
+  }
+  const newCat = req.body;
+  const newId = db.cats.push(newCat);
+  return res.status(201).send({
+    success: true,
+    message: 'Cat added successfully',
+    id: newId,
+  });
+});
+
+app.get('/cats', (req, res) => {
+  return res.status(200).send({
+    success: true,
+    data: db.cats.all(),
+  });
+});
+
+app.get('/cat/:id', (req, res) => {
+  const id = parseInt(req.params.id, 10);
+  const cat = db.cats.find({ id });
+  if (cat) {
+    return res.status(200).send({
+      success: true,
+      data: cat,
+    });
+  }
+  return res.status(404).send({
+    success: false,
+    message: 'Cat not found',
+  });
+});
+
+app.get('/catSearch/:key/:value', (req, res) => {
+  const { key, value } = req.params;
+  const cat = db.cats.find({ [key]: value });
+  if (cat) {
+    return res.status(200).send({
+      success: true,
+      data: cat,
+    });
+  }
+  return res.status(404).send({
+    success: false,
+    message: 'Cat not found',
+  });
+});
+
+//Pokemon
+app.post('/cat', (req, res) => {
+  if (!req.body.name) {
+    console.log(req.body);
+    return res.status(400).send({
+      success: false,
+      message: 'Name is required for cat',
+    });
+  }
+  const newCat = req.body;
+  const newId = db.cats.push(newCat);
+  return res.status(201).send({
+    success: true,
+    message: 'Cat added successfully',
+    id: newId,
+  });
+});
+
+app.get('/cats', (req, res) => {
+  return res.status(200).send({
+    success: true,
+    data: db.cats.all(),
+  });
+});
+
+app.get('/cat/:id', (req, res) => {
+  const id = parseInt(req.params.id, 10);
+  const cat = db.cats.find({ id });
+  if (cat) {
+    return res.status(200).send({
+      success: true,
+      data: cat,
+    });
+  }
+  return res.status(404).send({
+    success: false,
+    message: 'Cat not found',
+  });
+});
+
+app.get('/catSearch/:key/:value', (req, res) => {
+  const { key, value } = req.params;
+  const cat = db.cats.find({ [key]: value });
+  if (cat) {
+    return res.status(200).send({
+      success: true,
+      data: cat,
+    });
+  }
+  return res.status(404).send({
+    success: false,
+    message: 'Cat not found',
+  });
+});
+
+
 
 
 // Start server
