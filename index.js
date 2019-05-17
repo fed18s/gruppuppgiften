2ecffc9670911d53c361272f3a01520bb29e4c29
mhedengren <melker.hedengren@medieinstitutet.se>
2019-05-17 13:28:40 +0200

    Will add functionality for dogs and pokemons, linda.storgard@medieinstitutet.se tomasz.szczygiel@medieinstitutet.se

--- index.js
+++ index.js
@@ -16,7 +16,19 @@ db.addCollection('cats', [
   { name: 'Kitty', color: 'Grey', age: 1 },
 ]);
 
-// Setup the routes
+db.addCollection('dogs', [
+  { name: 'Pluto', color: 'Beiege', age: 7 },
+  { name: 'Rex', color: 'Gold', age: 4 },
+  { name: 'Kex', color: 'Grey', age: 2 },
+]);
+
+db.addCollection('pokemons', [
+  { name: 'Pikachu', color: 'Beiege', age: 7 },
+  { name: 'Squirtle', color: 'Gold', age: 4 },
+  { name: 'Slowpoke', color: 'Grey', age: 2 },
+]);
+
+// Setup the routes for cats
 app.post('/cat', (req, res) => {
   if (!req.body.name) {
     console.log(req.body);
@@ -68,6 +80,114 @@ app.get('/catSearch/:key/:value', (req, res) => {
   return res.status(404).send({
     success: false,
     message: 'Cat not found',
+  });
+});
+// Setup the routes for dogs
+app.post('/dog', (req, res) => {
+  if (!req.body.name) {
+    console.log(req.body);
+    return res.status(400).send({
+      success: false,
+      message: 'Name is required for dog',
+    });
+  }
+  const newDog = req.body;
+  const newId = db.dogs.push(newDog);
+  return res.status(201).send({
+    success: true,
+    message: 'Dog added successfully',
+    id: newId,
+  });
+});
+
+app.get('/dogs', (req, res) => {
+  return res.status(200).send({
+    success: true,
+    data: db.dogs.all(),
+  });
+});
+
+app.get('/dog/:id', (req, res) => {
+  const id = parseInt(req.params.id, 10);
+  const dog = db.dogs.find({ id });
+  if (dog) {
+    return res.status(200).send({
+      success: true,
+      data: dog,
+    });
+  }
+  return res.status(404).send({
+    success: false,
+    message: 'Dog not found',
+  });
+});
+
+app.get('/dogSearch/:key/:value', (req, res) => {
+  const { key, value } = req.params;
+  const dog = db.dogs.find({ [key]: value });
+  if (dog) {
+    return res.status(200).send({
+      success: true,
+      data: dog,
+    });
+  }
+  return res.status(404).send({
+    success: false,
+    message: 'Dog not found',
+  });
+});
+// Setup the routes for pokemons
+app.post('/pokemon', (req, res) => {
+  if (!req.body.name) {
+    console.log(req.body);
+    return res.status(400).send({
+      success: false,
+      message: 'Name is required for pokemon',
+    });
+  }
+  const newPokemon = req.body;
+  const newId = db.pokemons.push(newPokemon);
+  return res.status(201).send({
+    success: true,
+    message: 'Pokemon added successfully',
+    id: newId,
+  });
+});
+
+app.get('/pokemons', (req, res) => {
+  return res.status(200).send({
+    success: true,
+    data: db.pokemons.all(),
+  });
+});
+
+app.get('/pokemon/:id', (req, res) => {
+  const id = parseInt(req.params.id, 10);
+  const pokemon = db.pokemons.find({ id });
+  if (pokemon) {
+    return res.status(200).send({
+      success: true,
+      data: pokemon,
+    });
+  }
+  return res.status(404).send({
+    success: false,
+    message: 'Pokemon not found',
+  });
+});
+
+app.get('/pokemonSearch/:key/:value', (req, res) => {
+  const { key, value } = req.params;
+  const pokemon = db.pokemons.find({ [key]: value });
+  if (pokemon) {
+    return res.status(200).send({
+      success: true,
+      data: pokemon,
+    });
+  }
+  return res.status(404).send({
+    success: false,
+    message: 'Pokemons not found',
   });
 });
 
