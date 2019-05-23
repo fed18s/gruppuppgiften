5a5332e3da83893591d627705c5b1c615f37660d
JuliaLahdo <julia.lahdo@medieinstitutet.se>
2019-05-23 14:49:46 +0200

    Refactor code according to linter rules

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 import express from 'express';
 import bodyParser from 'body-parser';
 import Database from './lib/db';
-import Data from './animalData.js';
+import Data from './animalData';
 
 // Setup the server
 const PORT = 3000;
@@ -11,55 +11,53 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 // Setup the database
 const db = new Database();
-console.log("raw data: ", Data.cat);
+console.log('raw data: ', Data.cat);
 
 db.addCollection('cats', Data.cat);
 db.addCollection('pokemons', Data.pokemon);
 db.addCollection('dogs', Data.dog);
 
-function createAnimals(type, app, collection) {
- app.post('/'+type, (req, res) => {
-
-  //  let newId = db[type].push(req.body.animal);
-  let newId = collection.push(newId);
+function createAnimals(type, collection) {
+  app.post(`/${type}`, (req, res) => {
+    const newId = collection.push(req.data);
 
     if (!req.body.name) {
-    console.log(req.body);
-    return res.status(400).send({
-      success: false,
-      message: 'Name is required for ' + type,
+      console.log(req.body);
+      return res.status(400).send({
+        success: false,
+        message: `Name is required for ${type}`,
+      });
+    }
+
+    if (!req.body.age) {
+      console.log(req.body);
+      return res.status(400).send({
+        success: false,
+        message: `Age is required for ${type}`,
+      });
+    }
+
+    if (!req.body.color) {
+      console.log(req.body);
+      return res.status(400).send({
+        success: false,
+        message: `Color is required for ${type}`,
+      });
+    }
+
+    if (newId === 0) {
+      return res.status(404).send({
+        success: false,
+        message: `Oops! Something went wrong, adding ${type} was not successful.`,
+      });
+    }
+
+    return res.status(201).send({
+      success: true,
+      message: `${type} added successfully.`,
+      id: newId,
     });
-  }
-
-  if (!req.body.age) {
-    console.log(req.body);
-    return res.status(400).send({
-      success: false,
-      message: 'Age is required for ' + type,
-    });
-  }
-
-  if (!req.body.color) {
-    console.log(req.body);
-    return res.status(400).send({
-      success: false,
-      message: 'Color is required for ' + type,
-    });
-  }
-
-   if(newId === 0){
-     return res.status(404).send({
-       success: false,
-       message: 'Oops! Something went wrong, adding ' + type + ' was not successful.',
-     });
-   }
-
-   return res.status(201).send({
-     success: true,
-     message: type + ' added successfully.',
-     id: newId,
-   });
- });
+  });
 }
 
 // Setup the routes
@@ -137,17 +135,15 @@ function createAnimals(type, app, collection) {
 // type = cat/dog/pokemon
 // app = hela applikationen
 // collection = db.cats/db.pokemons/db.dogs
-function getAnimals(type, app, collection) {
-  app.get('/'+type+'s', (req, res) => {
-    return res.status(200).send({
-      success: true,
-      data: collection.all(),
-    });
-  });
+function getAnimals(type, collection) {
+  app.get(`/${type}s`, (req, res) => res.status(200).send({
+    success: true,
+    data: collection.all(),
+  }));
 }
 
-function getAnimalId(type, app, collection){
-  app.get('/'+type+'/:id', (req, res) => {
+function getAnimalId(type, collection) {
+  app.get(`/${type}/:id`, (req, res) => {
     const id = parseInt(req.params.id, 10);
     const animal = collection.find({ id });
     if (animal) {
@@ -158,47 +154,45 @@ function getAnimalId(type, app, collection){
     }
     return res.status(404).send({
       success: false,
-      message: type + ' not found',
+      message: `${type} not found`,
     });
   });
 }
 
 function searchAnimal(type, collection) {
-  app.get('/' + type + 'Search/:key/:value', (req, res) => {
+  app.get(`/${type}Search/:key/:value`, (req, res) => {
     const result = collection.find({ [req.params.key]: req.params.value });
 
     if (result) {
-    return res.status(200).send({
-      success: true,
-      data: result,
+      return res.status(200).send({
+        success: true,
+        data: result,
+      });
+    }
+    return res.status(404).send({
+      success: false,
+      message: `${type} not found`,
     });
-  }
-  return res.status(404).send({
-    success: false,
-    message: type + ' not found',
-  });
   });
 }
 
 [
-  {type: 'cat', collection: db.cats },
-  {type: 'dog', collection: db.dogs },
-  {type: 'pokemon', collection: db.pokemons },
+  { type: 'cat', collection: db.cats },
+  { type: 'dog', collection: db.dogs },
+  { type: 'pokemon', collection: db.pokemons },
 ].forEach((animal) => {
-  console.log("db: ", db.collections);
-  
-  console.log(animal.type + " : ", animal.collection);
-  
-  getAnimals(animal.type, app, animal.collection);
-  getAnimalId(animal.type, app, animal.collection);
-  createAnimals(animal.type, app, animal.collection);
+  console.log('db: ', db.collections);
+  console.log(`${animal.type}:`, animal.collection);
+  getAnimals(animal.type, animal.collection);
+  getAnimalId(animal.type, animal.collection);
+  createAnimals(animal.type, animal.collection);
   searchAnimal(animal.type, animal.collection);
 });
 
 // const animalArray = ["cat", "pokemon", "dog"];
 // animalArray.forEach((animal) => {
 
-//});
+// });
 
 // app.get('/catSearch/:key/:value', (req, res) => {
 //   const { key, value } = req.params;
