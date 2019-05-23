e158bca41b8b83a8673877c7bf834fa299ce613e
Filip Joelsson <filip.joelsson@weahead.se>
2019-05-24 00:49:52 +0200

    Added CORS support

--- app.js
+++ app.js
@@ -11,6 +11,11 @@ import animalData from './data/animalData';
 const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  next();
+});
 
 // Setup animal types
 const animalTypes = Object.keys(animalData);
