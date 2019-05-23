1a45ad60515cc8658e0b923b02b3ab8286b6fc13
Filip Joelsson <filip.joelsson@weahead.se>
2019-05-23 17:02:58 +0200

    Addded data for dogs and pokemon

--- data/animalData.js
+++ data/animalData.js
@@ -4,6 +4,18 @@ const cat = [
   { name: 'Kitty', color: 'Grey', age: 1 },
 ];
 
+const dog = [
+  { name: 'Nero', color: 'Black', breed: 'Pitbull' },
+  { name: 'Spot', color: 'Mixed', breed: 'Mixed' },
+];
+
+const pokemon = [
+  { name: 'Pikachu', color: 'Yellow', element: 'Electricity' },
+  { name: 'Whirlpool', color: 'Blue', element: 'Air' },
+];
+
 export default {
   cat,
+  dog,
+  pokemon,
 };
