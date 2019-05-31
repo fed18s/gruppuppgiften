931131ead9e2dab1de1e30131c9a1b6a87a2f8c8
feliciawallin <felicia.e.wallin@gmail.com>
2019-05-31 13:32:59 +0200

    fixed bug

--- index.js
+++ index.js
@@ -8,6 +8,11 @@ const PORT = 3000;
 const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use((req, res, next) => {
+ res.header('Access-Control-Allow-Origin', '*');
+ res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+ next();
+});
 
 // Setup the database
 const db = new Database();
